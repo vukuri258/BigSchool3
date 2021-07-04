820e779f5e1556d25a36c817cbb3a64aff14c8fc
Nguyen Tuan Vu <vukuri258@gmail.com>
2021-07-04 21:01:33 +0700

    Khoi tao du an .NET

---
+++ BigSchool3/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="BigSchool3.MvcApplication" Language="C#" %>
---
+++ BigSchool3/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(BigSchool3.Startup))]
+namespace BigSchool3
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
